a4e94329b9a8be65ddbaaf1b7a696f013ba2d142
Ayush Sharma <ayushsharma@Ayushs-MacBook-Air.local>
2024-10-09 15:35:25 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import SignUp from './signup/login/SignUp'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './signup/login/Login';
+import LandingPage from './LandingPage';
+import PrivateRoute from './PrivateRoute';
+import Dashboard from './Dashboard';
+import Employee from './user/Employee';
+import Admin from './user/Admin';
+import Userdetails from './Userdetails';
+
+
+const App = () => {
+  return (
+    <div>
+ <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage/>} />
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/sign" element={<SignUp/>}/>
+        <Route path='/edash' element={
+          <PrivateRoute>  <Employee/>
+          </PrivateRoute>
+        
+          }/>
+          <Route path='/adash' element={
+          <PrivateRoute>  <Admin/>
+          </PrivateRoute>
+        
+          }/>
+                <Route path="/details" element={<Userdetails/>}/>
+                
+
+      </Routes>
+
+    </Router>
+    </div>
+  )
+}
+
+export default App
